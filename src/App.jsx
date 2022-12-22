f1d79935c58292e4ff718fe45c425ea949e7e242
Neerajkumarc <chaudharyneeraj91@gmail.com>
2022-12-22 18:12:54 +0530

    optimize

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,14 @@ function App() {
   };
 
   return (
-    <div className="bg-[url('./assets/bg.jpg')] w-screen h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center">
+    <div className="bg-[url('./assets/bg.webp')] w-screen h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center">
       <div className="flex justify-center items-center gap-24 flex-col backdrop-blur-[1px] h-screen w-screen p-8 bg-gradient-to-b from-black via-[#31313188] to-transparent">
         <motion.img
           initial={{ opacity: 0, scale: 0.9 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1 }}
-          className="sm:w-[50%]"
+          width="640"
+          height="360"
           src={aq}
           alt="aq"
         />
@@ -50,7 +51,7 @@ function App() {
           animate={{ opacity: 1 }}
           transition={{ duration: 1 }}
           onClick={handleClick}
-          className="bg-black border-2 border-white text-white text-2xl px-12 p-2 rounded-lg font-bold active:scale-[.95] animate-pulse cursor-pointer"
+          className="bg-black border-2 border-white text-white text-2xl px-12 p-2 rounded-lg font-bold active:scale-[.95] cursor-pointer"
         >
           {quote == "" ? `Get Quotes 🔥` : ` Next`}
         </motion.button>
